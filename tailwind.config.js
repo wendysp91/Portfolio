4b1bab97ba1d438dc6f03c5d11615e3caf71f1a9
Wendy Suarez Pena <wendysp91@gmail.com>
2022-01-23 22:14:49 -0600

    Configure tailwind and create layout

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    container: {
+      center: true,
+      padding: '5rem',
+      screens: {
+        sm: '600px',
+        md: '728px',
+        lg: '984px',
+        xl: '1240px',
+        '2xl': '1496px',
+      },
+    }
+  },
+  plugins: [],
+}
